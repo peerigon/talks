0c720a75b7f9389c677b6f70843ab50fa7a6cff6
meaku <michael.jaser@peerigon.com>
2017-02-08 20:36:00 +0100

    Add current state

--- 2017-02-07-web-and-wine-js-on-hardware/demo/dashboard/dashboard.js
+++ 2017-02-07-web-and-wine-js-on-hardware/demo/dashboard/dashboard.js
@@ -1,14 +1,10 @@
-
-
 document.addEventListener("DOMContentLoaded", function(event) { 
 
     const liveChart = new LiveChart(document.getElementById("updating-chart"));
-
-    const sock = new SockJS("http://raspberrypi.local:8080/stream");
+    const sock = new SockJS("ws://raspberrypi.local:8080/stream");
 
     sock.onmessage = function(e) {
         const { ts, temperature } = JSON.parse(e.data);
         liveChart.addData(temperature, ts);
     };
 });
-        
