fb7a8d58680b03bd14b111df4c3b33b7c16a73a6
Johannes Ewald <mail@johannesewald.de>
2018-10-09 14:11:31 +0200

    Add 2018 09 28 hackerkiste webpack deep dive (#18)

---
+++ 2018-09-28-hackerkiste-webpack-deep-dive/examples/netscape-livewire.js
@@ -0,0 +1,10 @@
+
+<p>The budget is:
+<SCRIPT>
+<SERVER>
+write("var budget = " + client.amount);
+</SERVER>
+document.write(budget);
+</SCRIPT>
+</p>
+
---
+++ 2018-09-28-hackerkiste-webpack-deep-dive/examples/parse-domain.js
@@ -0,0 +1,9 @@
+
+
+import parseDomain from "parse-domain";
+
+const parsedDomain = parseDomain(new URL("http://example.com"));
+
+
+
+
